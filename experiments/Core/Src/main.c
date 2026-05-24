4f6dc41c2bc6bf180a2206c79f0100a207422ad4
Ayush <ayush4514.be23@chitkara.edu.in>
2026-05-25 00:08:59 +0530

    Add files via upload

--- experiments/Core/Src/main.c
+++ experiments/Core/Src/main.c
@@ -6,24 +6,27 @@
   ******************************************************************************
   */
 /* USER CODE END Header */
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stm32l475e_iot01.h"
-#include "stm32l475e_iot01_accelero.h"
-#include "stm32l475e_iot01_gyro.h"
-#include <string.h>
+#include "stm32l475e_iot01_tsensor.h"
 #include <stdio.h>
+#include <string.h>
 
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c1;
 UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
-char msg_motion[] = "LSM6DSL motion sensor initialized\r\n";
-char str_accel[100];
-char str_gyro[100];
-int16_t accel_data[3];
-float   gyro_data[3];
+#define SLAVE_ADDRESS_LCD 0x4E   // 0x27 << 1
+#define TEMP_THRESHOLD 28.0     // Temperature threshold set to 28°C
+
+float temp_value = 0;
+char uart_buffer[100];
+char lcd_line1[20];
+char lcd_line2[20];
+uint8_t msg3[] = "=====> Temperature sensor HTS221 initialized \r\n ";
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -31,12 +34,75 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_USART1_UART_Init(void);
-void Error_Handler(void);
 
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
+/* USER CODE BEGIN 0 */
+/* ================= LCD FUNCTIONS ================= */
+
+void lcd_send_cmd(char cmd)
+{
+    char data_u, data_l;
+    uint8_t data_t[4];
+
+    data_u = (cmd & 0xf0);
+    data_l = ((cmd << 4) & 0xf0);
+
+    data_t[0] = data_u | 0x0C;
+    data_t[1] = data_u | 0x08;
+    data_t[2] = data_l | 0x0C;
+    data_t[3] = data_l | 0x08;
+
+    HAL_I2C_Master_Transmit(&hi2c1, SLAVE_ADDRESS_LCD, data_t, 4, 100);
+    HAL_Delay(2);
+}
+
+void lcd_send_data(char data)
+{
+    char data_u, data_l;
+    uint8_t data_t[4];
+
+    data_u = (data & 0xf0);
+    data_l = ((data << 4) & 0xf0);
+
+    data_t[0] = data_u | 0x0D;
+    data_t[1] = data_u | 0x09;
+    data_t[2] = data_l | 0x0D;
+    data_t[3] = data_l | 0x09;
+
+    HAL_I2C_Master_Transmit(&hi2c1, SLAVE_ADDRESS_LCD, data_t, 4, 100);
+    HAL_Delay(1);
+}
+
+void lcd_init(void)
+{
+    HAL_Delay(50);
+    lcd_send_cmd(0x30);
+    HAL_Delay(5);
+    lcd_send_cmd(0x30);
+    HAL_Delay(5);
+    lcd_send_cmd(0x30);
+    HAL_Delay(5);
+    lcd_send_cmd(0x20);
+
+    lcd_send_cmd(0x28);
+    lcd_send_cmd(0x08);
+    lcd_send_cmd(0x01);
+    HAL_Delay(2);
+    lcd_send_cmd(0x06);
+    lcd_send_cmd(0x0C);
+}
+
+void lcd_set_cursor(uint8_t col, uint8_t row)
+{
+    uint8_t address = (row == 0) ? (0x80 + col) : (0xC0 + col);
+    lcd_send_cmd(address);
+}
+
+void lcd_send_string(char *str)
+{
+    while (*str) lcd_send_data(*str++);
+}
+/* USER CODE END 0 */
+
 int main(void)
 {
   HAL_Init();
@@ -46,178 +112,92 @@ int main(void)
   MX_I2C1_Init();
   MX_USART1_UART_Init();
 
-  /* Init sensors */
-  if (BSP_ACCELERO_Init() != ACCELERO_OK || BSP_GYRO_Init() != GYRO_OK)
-  {
-    char errmsg[] = "Motion sensor Init Error\r\n";
-    HAL_UART_Transmit(&huart1, (uint8_t*)errmsg, strlen(errmsg), 1000);
-    while (1);
-  }
-  else
-  {
-    HAL_UART_Transmit(&huart1, (uint8_t*)msg_motion, strlen(msg_motion), 1000);
-  }
-
-  HAL_Delay(100);
+  /* USER CODE BEGIN 2 */
+  BSP_TSENSOR_Init();
+  HAL_UART_Transmit(&huart1, msg3, sizeof(msg3), 1000);
+  lcd_init();
+  /* USER CODE END 2 */
 
   /* Infinite loop */
   while (1)
   {
-    /* On-board USER button is on PC13 and is ACTIVE LOW */
-    while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_SET)
-    {
-      /* Do nothing, wait until button is pressed */
-    }
+      temp_value = BSP_TSENSOR_ReadTemp();
 
-    /* Debounce */
-    HAL_Delay(200);
+      /* Temperature Threshold Alert Logic */
+      if(temp_value >= TEMP_THRESHOLD)
+      {
+          /* LED ON */
+          HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_SET);
 
-    /* ---- Stream data UNTIL button is pressed again ---- */
-    while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_SET)
-    {
-      BSP_ACCELERO_AccGetXYZ(accel_data);
-      sprintf(str_accel, "Accelero: X=%.2f, Y=%.2f, Z=%.2f mg\r\n",
-              (float)accel_data[0] / 1000.0f,
-              (float)accel_data[1] / 1000.0f,
-              (float)accel_data[2] / 1000.0f);
-      HAL_UART_Transmit(&huart1, (uint8_t*)str_accel, strlen(str_accel), 1000);
+          /* Buzzer ON */
+          HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, GPIO_PIN_SET);
 
-      HAL_Delay(1000);
+          snprintf(lcd_line2, sizeof(lcd_line2), "ALERT: HOT!");
+      }
+      else
+      {
+          /* LED OFF */
+          HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_RESET);
 
-      BSP_GYRO_GetXYZ(gyro_data);
-      sprintf(str_gyro, "Gyro: X=%.2f, Y=%.2f, Z=%.2f mdps\r\n",
-              gyro_data[0], gyro_data[1], gyro_data[2]);
-      HAL_UART_Transmit(&huart1, (uint8_t*)str_gyro, strlen(str_gyro), 1000);
+          /* Buzzer OFF */
+          HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, GPIO_PIN_RESET);
 
-      HAL_Delay(1000);
-    }
+          snprintf(lcd_line2, sizeof(lcd_line2), "Temp Normal");
+      }
 
-    /* ---- Button pressed again -> STOP streaming ---- */
-    HAL_Delay(200);  // debounce
+      /* UART Output */
+      snprintf(uart_buffer, sizeof(uart_buffer),
+               "Temp: %.2f C\r\n", temp_value);
 
-    /* Wait until button is released before next cycle */
-    while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_RESET);
-    HAL_Delay(50);
+      HAL_UART_Transmit(&huart1,
+                        (uint8_t*)uart_buffer,
+                        strlen(uart_buffer),
+                        1000);
+
+      /* LCD Line 1 */
+      snprintf(lcd_line1, sizeof(lcd_line1),
+               "Temp: %.2f C", temp_value);
+
+      lcd_set_cursor(0,0);
+      lcd_send_string("                ");
+      lcd_set_cursor(0,0);
+      lcd_send_string(lcd_line1);
+
+      /* LCD Line 2 */
+      lcd_set_cursor(0,1);
+      lcd_send_string("                ");
+      lcd_set_cursor(0,1);
+      lcd_send_string(lcd_line2);
+
+      HAL_Delay(500);
   }
 }
 
-/**
-  * @brief System Clock Configuration
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 1;
-  RCC_OscInitStruct.PLL.PLLN = 10;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
-  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
-  {
-    Error_Handler();
-  }
-}
-
-/**
-  * @brief I2C1 Initialization Function
-  */
-static void MX_I2C1_Init(void)
-{
-  hi2c1.Instance = I2C1;
-  hi2c1.Init.Timing = 0x10D19CE4;
-  hi2c1.Init.OwnAddress1 = 0;
-  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2 = 0;
-  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
-  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
-  {
-    Error_Handler();
-  }
-}
-
-/**
-  * @brief USART1 Initialization Function
-  */
-static void MX_USART1_UART_Init(void)
-{
-  huart1.Instance = USART1;
-  huart1.Init.BaudRate = 115200;
-  huart1.Init.WordLength = UART_WORDLENGTH_8B;
-  huart1.Init.StopBits = UART_STOPBITS_1;
-  huart1.Init.Parity = UART_PARITY_NONE;
-  huart1.Init.Mode = UART_MODE_TX_RX;
-  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&huart1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-}
-
-/**
-  * @brief GPIO Initialization Function
-  */
+/* ================= GPIO INIT ================= */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /* Configure PC13 as input (On-board USER button, active LOW) */
+  /* LED PB14 */
+  GPIO_InitStruct.Pin = GPIO_PIN_14;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* Buzzer PB15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* Button PC13 */
   GPIO_InitStruct.Pin = GPIO_PIN_13;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-}
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  */
-void Error_Handler(void)
-{
-  __disable_irq();
-  while (1)
-  {
-  }
 }
